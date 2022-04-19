4ba62d438ab8aee974bffda8ed4eb8dff50c7f9e
Yuan Zhang <zhangyuan@users.noreply.github.com>
2022-04-19 14:00:15 +0800

    display job details

--- cmd/glue.go
+++ cmd/glue.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
 	"os"
 
@@ -24,29 +26,69 @@ func main() {
 }
 
 func run(jobName string) error {
-	sess := session.Must(session.NewSession(&aws.Config{
+	sess, err := session.NewSession(&aws.Config{
 		MaxRetries: aws.Int(3),
-	}))
+	})
+	if err != nil {
+		return err
+	}
 
 	svc := glue.New(sess)
+
+	jobOutput, err := svc.GetJob(&glue.GetJobInput{
+		JobName: aws.String(jobName),
+	})
+	if err != nil {
+		return errors.Wrap(err, "fail to get job")
+	}
+
+	renderJob(jobOutput)
 
 	runsOutput, err := svc.GetJobRuns(&glue.GetJobRunsInput{
 		JobName:    aws.String(jobName),
 		MaxResults: aws.Int64(10),
 	})
 
+	renderJobRuns(*runsOutput)
+
 	if err != nil {
 		return errors.Wrap(err, "fail to batch get jobs")
 	}
+	return nil
+}
 
+func renderJob(jobOutput *glue.GetJobOutput) {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.AppendRow([]interface{}{"Name", *jobOutput.Job.Name})
+	t.AppendSeparator()
+	t.AppendRow([]interface{}{"Description", *jobOutput.Job.Description})
+	t.AppendSeparator()
+	t.AppendRow([]interface{}{"CreatedOn", jobOutput.Job.CreatedOn.Local()})
+	t.AppendSeparator()
+	t.AppendRow([]interface{}{"LastModifiedOn", jobOutput.Job.LastModifiedOn.Local()})
+	t.AppendSeparator()
+
+	var buffer bytes.Buffer
+	for key, element := range jobOutput.Job.DefaultArguments {
+		buffer.WriteString(fmt.Sprintf("%s: %s\n", key, *element))
+	}
+
+	t.AppendRow([]interface{}{"DefaultArguments", buffer.String()})
+
+	t.SetStyle(table.StyleLight)
+	t.Render()
+}
+
+func renderJobRuns(runsOutput glue.GetJobRunsOutput) {
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
 	t.AppendHeader(table.Row{"#", "Run Id", "State", "Completed On"})
 
 	for index, jr := range runsOutput.JobRuns {
 		t.AppendRow([]interface{}{index, *jr.Id, *jr.JobRunState, jr.CompletedOn.Local()})
+		t.AppendSeparator()
 	}
 	t.SetStyle(table.StyleLight)
 	t.Render()
-	return nil
 }
